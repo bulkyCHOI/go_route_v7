0e437665d5c4780eeb314ad1d370eb6264dc4a1c
jaesung <ppaccomy@gmail.com>
2023-09-01 15:58:20 +0900

    230901 | 섹션13-3 Go함수

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,3 +1,4 @@
+import 'package:go_route_v7/screen/1_basic_screen.dart';
 import 'package:go_route_v7/screen/root_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,6 +10,15 @@ final router = GoRouter(
       builder: (context, state) {
         return RootScreen();
       },
+      //중첩이 가능하다.
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) {
+            return BasicScreen();
+          },
+        ),
+      ],
     ),
   ],
 );
--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_route_v7/layout/default_layout.dart';
+import 'package:go_router/go_router.dart';
 
 class RootScreen extends StatelessWidget {
   const RootScreen({Key? key}) : super(key: key);
@@ -8,7 +9,14 @@ class RootScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       body: ListView(
-        children: [],
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              context.go('/basic');
+            },
+            child: Text('Go Basic'),
+          ),
+        ],
       ),
     );
   }
